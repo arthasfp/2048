d6fee4e295fb130eab5713e56a12e1d99baf4c9a
arthasfp <arthasfp@mail.ru>
2015-05-28 09:18:36 +0400

    Field

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -5,4 +5,13 @@ public class Field {
     Row[] rows;
     Column[] columns;
 
+    public Field(int sizeOfField) {
+        Cell[][] cells = new Cell[sizeOfField][sizeOfField];
+
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells.length; j++) {
+                cells[i][j] = new Cell(0);
+            }
+        }
+    }
 }
